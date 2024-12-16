5a06327e1339e811c0fb576b0b4d7e1803e3f3f0
wataru55 <wataru200155@gmail.com>
2024-12-17 00:47:26 +0900

    Initial Commit

---
+++ Fenrir/FenrirApp.swift
@@ -0,0 +1,17 @@
+//
+//  FenrirApp.swift
+//  Fenrir
+//
+//  Created by 高橋和 on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct FenrirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FenrirTests/FenrirTests.swift
@@ -0,0 +1,17 @@
+//
+//  FenrirTests.swift
+//  FenrirTests
+//
+//  Created by 高橋和 on 2024/12/16.
+//
+
+import Testing
+@testable import Fenrir
+
+struct FenrirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
