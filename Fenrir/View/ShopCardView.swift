10e07bed83103f6664e7cba5f7b33540e4287c63
wataru55 <wataru200155@gmail.com>
2024-12-19 00:55:30 +0900

    add: 店舗一覧を表示するためのビューを作成

---
+++ Fenrir/View/ShopCardView.swift
@@ -0,0 +1,46 @@
+//
+//  ShopCardView.swift
+//  Fenrir
+//
+//  Created by 高橋和 on 2024/12/17.
+//
+
+import SwiftUI
+
+struct ShopCardView: View {
+    let shop: Shop
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.black)
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(shop.name)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.black)
+                    .padding(.bottom, 10)
+                
+                Text(shop.mobileAccess)
+                    .font(.system(size: 15))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.gray)
+            }
+            .padding()
+        }
+        .frame(width: UIScreen.main.bounds.width, height: 150)
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .padding()
+                .foregroundStyle(.white)
+                .shadow(color: .black.opacity(0.5), radius: 2, x: 2, y: 2)
+        )
+    }
+}
+
+#Preview {
+    ShopCardView(shop: Shop(id: "1", name: "白木屋", logoImage: "a", address: "折尾駅周辺", lat: 33.3, lng: 180.564, mobileAccess: "折尾駅から徒歩２分", open: "16:00~", close: "~23:00"))
+}
