1cdd5ec7e05defe12d495b5e41374cef861cad01
wataru55 <wataru200155@gmail.com>
2024-12-20 12:21:49 +0900

    fix: 位置情報の更新を自動から手動にするための変更

--- Fenrir/Model/LocationManager.swift
+++ Fenrir/Model/LocationManager.swift
@@ -16,12 +16,20 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         super.init()
         self.locationManager.delegate = self
         self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
+        self.requestCurrentLocation()
     }
+    
+    func requestCurrentLocation() {
+            self.locationManager.requestLocation()
+        }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             self.location = location
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("位置情報の取得に失敗しました: \(error)")
     }
 }
